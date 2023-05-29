828eeb77153a6fb6dff2d7ce9c8e36822bbfe114
Miguel Hojman <miguelhojman@gmail.com>
2023-05-29 18:51:35 -0300

    estilos responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
@@ -10,8 +11,19 @@ import { PersonajesComponent } from './components/personajes/personajes.componen
 import { IndividualComponent } from './components/individual/individual.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, MainComponent, PersonajesComponent, IndividualComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    MainComponent,
+    PersonajesComponent,
+    IndividualComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
